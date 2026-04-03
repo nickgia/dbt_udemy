eb8804415414193dfade0ffe361feb5eea5a7c82
ngiambla@gmail.com <ngiambla@gmail.com>
2026-04-03 12:25:34 +0000

    developping stg_orders

---
+++ models/example/staging/stg_orders.sql
@@ -0,0 +1,27 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+select 
+--from raw orders 
+o.orderid,
+o.orderdate,
+o.shipmode,
+o.ordersellingprice - ordercostprice as orderprofit,
+o.ordercostprice,
+o.ordersellingprice,
+--from raw Customer 
+c.customername,
+c.segment,
+c.country,
+--from raw product
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_order') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p
+on o.productid= p.productid
