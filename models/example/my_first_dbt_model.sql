1c374a5933692255db3efc07715935c48809bde8
ngiambla@gmail.com <ngiambla@gmail.com>
2026-04-03 09:04:58 +0000

    change first model

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,3 +1,9 @@
-{% for i in range(10) %}
-  select {{ i }} as number {% if not loop.last %} union all {% endif %}
-{% endfor %}
+{{config(materialized='table')}}
+with source_data as (
+    select 1 as id
+    union all
+    select null as id
+)
+    
+
+select * from source_data
