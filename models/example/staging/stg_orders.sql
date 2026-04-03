0bd11739e98f8fad1e59c4c2e4ffb6edcc08b37b
ngiambla@gmail.com <ngiambla@gmail.com>
2026-04-03 14:40:23 +0000

    Raw_product raw_customer raw_order

--- models/example/staging/stg_orders.sql
+++ models/example/staging/stg_orders.sql
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 {{
     config(
         materialized='table'
@@ -13,24 +13,11 @@ o.shipmode,
 o.ordersellingprice - ordercostprice as orderprofit,
 o.ordercostprice,
 o.ordersellingprice,
---from raw Customer 
-=======
-select 
---from raw orders
-o.orderid,
-o.orderdate,
-o.shipdate,
-o.shipmode,
-o.ordersellingprice - o.ordercostprice as orderprofit,
-o.ordersellingprice ,
-o.ordercostprice,
---from raw customer
->>>>>>> 7918133eeb9be4a0cc1404775f28c1d260725123
+--from raw Customer
 c.customerid,
 c.customername,
 c.segment,
 c.country,
-<<<<<<< HEAD
 --from raw product
 p.productid,
 p.category,
@@ -41,15 +28,3 @@ left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid= p.productid
-=======
---from rwa product
-p.productid,
-p.productname,
-p.category,
-p.subcategory
-from {{ ref('raw_orders') }} as o
-left join {{ ref('raw_customer') }} as c
-on o.customerid = c.customerid
-left join {{ ref('raw_product') }} as p
-on o.productid =p.productid
->>>>>>> 7918133eeb9be4a0cc1404775f28c1d260725123
